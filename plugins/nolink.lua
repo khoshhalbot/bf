f8089baff696b2a0c2f1d650908d58280f4d5d46
behradhg <BEHRADBOT@GMaIL.COM>
2016-02-18 12:34:13 +0330

    Create nolink.lua

---
+++ plugins/nolink.lua
@@ -0,0 +1,24 @@
+
+do
+
+function run(msg, matches)
+  return [[❌⛔️ تبلیغ ممتوع❌⛔️
+]
+end
+
+return {
+  description = "", 
+  usage = "",
+  patterns = {
+  "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp]://",
+"[Ww][Ww][Ww]:",
+  }, 
+  run = run 
+}
+
+end
+
